da5f7ce285e4969af5ad9ce2f1fba75a25a42373
Kirvvs <jb.ursua13@gmail.com>
2023-07-07 00:22:19 +0800

    Game Rendering

--- Controller/Game.cpp
+++ Controller/Game.cpp
@@ -2,28 +2,45 @@
 
 using namespace controllers;
 
-Game::Game() : rwWindow(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Title", sf::Style::Titlebar | sf::Style::Close) {
-    this->rwWindow.setFramerateLimit(FRAME_RATE_LIMIT);
+
+Game::Game() : rwWindow(sf::VideoMode(TILE_HEIGHT * GRID_SIZE, TILE_HEIGHT * GRID_SIZE), "Title", sf::Style::Titlebar | sf::Style::Close) {
+    this->rwWindow.setFramerateLimit(60);
     std::srand(std::time(NULL));
 
-    //TextureManager::getInstance()->loadAll();
+    for(int i = 0; i < GRID_SIZE; i++){
+        for(int j = 0; j < GRID_SIZE; j++){
+            sf::RectangleShape* rectangle = new sf::RectangleShape(sf::Vector2f(TILE_WIDTH, TILE_HEIGHT));
+            if(this->grid[i][j] == 1){
+                rectangle->setFillColor(sf::Color(123, 63, 0, 255));
+            }
+            else{
+                rectangle->setFillColor(sf::Color(255,255,255,5));
+            }
+            rectangle->setPosition(TILE_WIDTH * j, TILE_HEIGHT * i);
+            this->vecTiles.push_back(rectangle);
 
-    this->createNullObjectComponents();
+            if(i == this->TankY && j == this->TankX){
+                this->TankShape = new sf::RectangleShape(sf::Vector2f(TANK_WIDTH, TANK_HEIGHT));
+                this->TankShape->setFillColor(sf::Color(0,255,0,50));
+                this->TankShape->setPosition(TILE_WIDTH * j + TANK_OFFSET/2, TILE_HEIGHT * i + TANK_OFFSET/2);
+            }
 
-    //Area* pArea = new MarbleGallery();
-    //GameObjectManager::getInstance()->addObject(pArea);
-    //GameObjectManager::getInstance()->addObject(this->pPhysicsManagerHolder);
-}
+            if(i == this->ETankY && j == this->ETankX){
+                this->ETankShape = new sf::RectangleShape(sf::Vector2f(TANK_WIDTH, TANK_HEIGHT));
+                this->ETankShape->setFillColor(sf::Color(210, 180, 140,255));
+                this->ETankShape->setPosition(TILE_WIDTH * j + TANK_OFFSET/2, TILE_HEIGHT * i + TANK_OFFSET/2);
+                
+            }
+        }
+    }
+    
 
-void Game::createNullObjectComponents() {
-    //this->pPhysicsManagerHolder = new EmptyGameObject("Physics Manager Holder");
-    //PhysicsManager::initialize("Physics Manager System", this->pPhysicsManagerHolder);
 }
 
 void Game::run() {
     sf::Clock CClock = sf::Clock();
     sf::Time tLastUpdate = sf::Time::Zero;
-    sf::Time tTimePerFrame = sf::seconds(1.0f / FRAME_RATE_LIMIT);
+    sf::Time tTimePerFrame = sf::seconds(1.0f / 60);
 
     while(this->rwWindow.isOpen()) {
         this->processEvents();
@@ -46,20 +63,26 @@ void Game::processEvents() {
             case sf::Event::Closed:
                 this->rwWindow.close();
                 break;
-  
+
+            // Placed to remove the warning.    
             default:
-                //GameObjectManager::getInstance()->processInput(eEvent);
                 break;
         }
     }
 }
 
 void Game::update(sf::Time tDeltaTime) {
-    //GameObjectManager::getInstance()->update(tDeltaTime);
+    
 }
 
 void Game::render() {
     this->rwWindow.clear();
-    //GameObjectManager::getInstance()->draw(&this->rwWindow);
+    
+    for(sf::RectangleShape *tile : this->vecTiles){
+        this->rwWindow.draw(*tile);
+    }
+    this->rwWindow.draw(*(this->TankShape));
+    this->rwWindow.draw(*(this->ETankShape));
+
     this->rwWindow.display();
 }
---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+#include "cstdlib"
+#include "cmath"
+
+#include "SFML/Graphics.hpp"
+
+#include "Controller/Game.cpp"
+#include "Config/Config.h"
+
+
+int main(){
+
+    Game CGame;
+    CGame.run();
+
+    return 0;
+}
+
